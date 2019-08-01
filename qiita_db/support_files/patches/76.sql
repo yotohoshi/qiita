c0fad6bed7121c8493d3a2441ef941abc4d6c471
Isaac Jiang <isaacjiang@gmail.com>
2019-07-31 18:24:21 -0700

    test

--- qiita_db/support_files/patches/76.sql
+++ qiita_db/support_files/patches/76.sql
@@ -1,5 +1,5 @@
 -- Jul 29 2019
 
-ALTER TABLE qiita.qiita_user DROP CONSTRAINT pk_user;
+ALTER TABLE qiita.qiita_user DROP CONSTRAINT pk_user CASCADE;
 
--- ALTER TABLE qiita.qiita_user ADD qiita_user_id BIGSERIAL;
+ALTER TABLE qiita.qiita_user ADD qiita_user_id BIGSERIAL PRIMARY KEY;
